7b3adef5a8118b511619799d8582df70695351e0
rdhwan <fathanridhwann@gmail.com>
2024-04-19 22:01:37 +0700

    fix: simplify verifikasi
    gabung verifikasi, jadi tergantung role di req body

--- src/controllers/verifikasi.controller.ts
+++ src/controllers/verifikasi.controller.ts
@@ -8,83 +8,65 @@ import {
   validationError,
   parseZodError,
 } from "@/utils/responses";
-import { idSchema } from "@/models/id.model";
-import { verifikasiSchema } from "@/models/verifikasi.model";
+import { verifikasiSchema, type Verifikasi } from "@/models/verifikasi.model";
 
-export const verifikasiPanitia = async (req: Request, res: Response) => {
+export const verifikasi = async (
+  req: Request<{}, {}, Verifikasi>,
+  res: Response
+) => {
   try {
-    const validateId = await idSchema.safeParseAsync(req.params.id);
-    if (!validateId.success) {
-      return validationError(res, parseZodError(validateId.error));
-    }
     const validateBody = await verifikasiSchema.safeParseAsync(req.body);
     if (!validateBody.success) {
       return validationError(res, parseZodError(validateBody.error));
     }
 
-    const panitia = await db.panitia.findUnique({
-      where: {
-        id: validateId.data,
-      },
-    });
+    if (req.body.role === "panitia") {
+      const panitia = await db.panitia.findUnique({
+        where: {
+          id: validateBody.data.id,
+        },
+      });
 
-    if (!panitia) {
-      logging("ERROR", `Panitia not found with id ${req.params.id}`);
-      return notFound(res, `Panitia with id ${req.params.id} did not exist`);
-    }
+      if (!panitia) {
+        logging("ERROR", `Panitia not found with id ${validateBody.data.id}`);
+        return notFound(
+          res,
+          `Panitia with id ${validateBody.data.id} did not exist`
+        );
+      }
 
-    const updatedPanitia = await db.panitia.update({
-      where: {
-        id: validateId.data,
-      },
-      data: {
-        isVerified: !panitia.isVerified,
-      },
-    });
+      const updatedPanitia = await db.panitia.update({
+        where: {
+          id: validateBody.data.id,
+        },
+        data: {
+          isVerified: !panitia.isVerified,
+        },
+      });
 
-    return success(
-      res,
-      `Panitia with id ${req.params.id} verified successfully`,
-      updatedPanitia
-    );
-  } catch (err) {
-    logging(
-      "ERROR",
-      `Error trying to verified Panitia with id ${req.params.id}`,
-      err
-    );
-    return internalServerError(res);
-  }
-};
-
-export const verifikasiOrganisator = async (req: Request, res: Response) => {
-  try {
-    const validateId = await idSchema.safeParseAsync(req.params.id);
-    if (!validateId.success) {
-      return validationError(res, parseZodError(validateId.error));
-    }
-    const validateBody = await verifikasiSchema.safeParseAsync(req.body);
-    if (!validateBody.success) {
-      return validationError(res, parseZodError(validateBody.error));
+      return success(
+        res,
+        `Panitia with id ${validateBody.data.id} verified successfully`
+      );
     }
 
     const organisator = await db.organisator.findUnique({
       where: {
-        id: validateId.data,
+        id: validateBody.data.id,
       },
     });
 
     if (!organisator) {
-      logging("ERROR", `Organisator not found with id ${req.params.id}`);
+      logging("ERROR", `Organisator not found with id ${validateBody.data.id}`);
       return notFound(
         res,
-        `Organisator with id ${req.params.id} did not exist`
+        `Organisator with id ${validateBody.data.id} did not exist`
       );
     }
 
     const updatedOrganisator = await db.organisator.update({
       where: {
-        id: validateId.data,
+        id: validateBody.data.id,
       },
       data: {
         isVerified: !organisator.isVerified,
@@ -93,15 +75,10 @@ export const verifikasiOrganisator = async (req: Request, res: Response) => {
 
     return success(
       res,
-      `Organisator with id ${req.params.id} verified successfully`,
-      updatedOrganisator
+      `Organisator with id ${validateBody.data.id} verified successfully`
     );
   } catch (err) {
-    logging(
-      "ERROR",
-      `Error trying to verified Organisator with id ${req.params.id}`,
-      err
-    );
+    logging("ERROR", `Error trying to verify user`, err);
     return internalServerError(res);
   }
 };
--- src/routes/verifikasi.route.ts
+++ src/routes/verifikasi.route.ts
@@ -1,31 +1,18 @@
 import { Router } from "express";
-
-import {
-  verifikasiPanitia,
-  verifikasiOrganisator,
-} from "@/controllers/verifikasi.controller";
+import { verifikasi } from "@/controllers/verifikasi.controller";
 import verifyJwt from "@/middlewares/verifyJwt.middleware";
 import verifyRole from "@/middlewares/verifyRole.middleware";
 import verifyDivisiPanitia from "@/middlewares/verifyDivisiPanitia.middleware";
 
 const router = Router();
 
-// verifikasi Panitia
+// verifikasi
 router.put(
-  "/verifikasiPanitia/:id",
+  "/verifikasi",
   verifyJwt,
   verifyRole(["panitia"]),
   verifyDivisiPanitia([1, 2]),
-  verifikasiPanitia
-);
-
-// verifikasi Organisator
-router.put(
-  "/verifikasiOrganisator/:id",
-  verifyJwt,
-  verifyRole(["panitia"]),
-  verifyDivisiPanitia([1, 2]),
-  verifikasiOrganisator
+  verifikasi
 );
 
 export default router;
