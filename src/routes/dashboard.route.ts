ad49f447af2c6fb0d8cd30303c24a7330f554172
rdhwan <fathanridhwann@gmail.com>
2024-04-27 19:35:51 +0700

    feat: add organisator statistic

--- src/routes/dashboard.route.ts
+++ src/routes/dashboard.route.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import verifyJwt from "@/middlewares/verifyJwt.middleware";
 import verifyRole from "@/middlewares/verifyRole.middleware";
-import { dashboardStatistics } from "@/controllers/dashboard.controller";
+import {
+  dashboardOrganisator,
+  dashboardStatistics,
+} from "@/controllers/dashboard.controller";
 
 const router = Router();
 
-
-router.get("/", verifyJwt, verifyRole(["panitia"]), dashboardStatistics);
-
+router.get("/panitia", verifyJwt, verifyRole(["panitia"]), dashboardStatistics);
+router.get(
+  "/organisator",
+  verifyJwt,
+  verifyRole(["organisator"]),
+  dashboardOrganisator
+);
 
 export default router;
