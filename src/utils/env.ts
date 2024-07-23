df9f65d5ec9360f9b67222a7890da93a48be2b4a
rdhwan <fathanridhwann@gmail.com>
2024-07-23 17:38:27 +0700

    feat: implement mail queue

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -8,6 +8,7 @@ const envs = [
   "APP_DB_URL",
   "APP_API_URL",
   "APP_FRONTEND_URL",
+  "APP_MQ_URL",
   "R2_ACCOUNT_ID",
   "R2_ACCESS_KEY_ID",
   "R2_SECRET_ACCESS_KEY",
@@ -39,13 +40,14 @@ const ENV = {
   APP_DB_URL: Bun.env.APP_DB_URL || "mysql://root@localhost:3306/mxm24_db",
   APP_API_URL: Bun.env.APP_API_URL || "http://localhost:8080",
   APP_FRONTEND_URL: Bun.env.APP_FRONTEND_URL || "http://localhost:5173",
+  APP_MQ_URL: Bun.env.APP_MQ_URL || "http://localhost:8085/mail",
   R2_ACCOUNT_ID: Bun.env.R2_ACCOUNT_ID || "R2_ACCOUNT_ID",
   R2_ACCESS_KEY_ID: Bun.env.R2_ACCESS_KEY_ID || "R2_ACCESS_KEY_ID",
   R2_SECRET_ACCESS_KEY: Bun.env.R2_SECRET_ACCESS_KEY || "R2_SECRET_ACCESS_KEY",
   R2_BUCKET_NAME: Bun.env.R2_BUCKET_NAME || "R2_BUCKET_NAME",
   R2_PUBLIC_URL: Bun.env.R2_PUBLIC_URL || "R2_PUBLIC_URL",
   MIDTRANS_SERVER_KEY: Bun.env.MIDTRANS_SERVER_KEY || "midtrans",
-  MIDTRANS_ENV: Bun.env.MIDTRANS_ENV || "sandbox"
+  MIDTRANS_ENV: Bun.env.MIDTRANS_ENV || "sandbox",
 };
 
 export default ENV;
